51ef4453f5f01de48fa6fe98a5d6e409d628970a
StefanyaSM <stefanyasalm@gmail.com>
2026-02-22 16:22:07 -0600

    Elimine todo lo innecesario y usuario=stefanya

--- Prueba_Template/src/main/java/com/fing/app/controllers/MainCtrl.java
+++ Prueba_Template/src/main/java/com/fing/app/controllers/MainCtrl.java
@@ -7,44 +7,45 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class MainCtrl {
-
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-
-	
-	
-	
-	@GetMapping("/p")
+	@GetMapping("/")
 	public String raiz(Model model){
 		
+		
 		ArrayList<Order> lista= new ArrayList<>();
-		lista.add(new Order());
-		lista.add(new Order());
-		lista.add(new Order());
-		lista.add(new Order());
-		lista.add(new Order());
 		
-		
-		model.addAttribute("Ordenes", lista);
-		Order o1= new Order();
-		
+		lista.add(new Order());
+	    lista.add(new Order());
+	    lista.add(new Order());
+	    lista.add(new Order());
+	    lista.add(new Order());
+
+	    model.addAttribute("ordenes", lista);
+	    model.addAttribute("msg", "Hola");
+	    
+	    
+	    //Nombre de usuario= stefanya saldaña 
+	    //la parte junto a la foto
+	    Order usuario = new Order();
+	    usuario.setUsuario("Stefanya Saldaña");
+
+	    model.addAttribute("usuarioNom", usuario.getUsuario());
+        
 
 		
 		
-		model.addAttribute("msg", "Hola");
+		
 		return "index";
 	}
 	
 	
 	
 }
+
+
+
--- Prueba_Template/src/main/java/com/fing/app/models/Order.java
+++ Prueba_Template/src/main/java/com/fing/app/models/Order.java
@@ -3,6 +3,16 @@ package com.fing.app.models;
 public class Order {
 	
 	private int folio= 0;
+	private String usuario;
+
+	
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
 
 	public int getFolio() {
 		return folio;
